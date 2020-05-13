57a3f072b8dd2f0170c1a6064702346d611255f2
Delaney McKnight <delaneymcknight@Delaneys-MacBook-Pro-2.local>
2020-05-12 20:48:00 -0700

    Image gallery & template html/scss/jscript, background/text formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 console.log('Hello from js/scripts.js!');
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
