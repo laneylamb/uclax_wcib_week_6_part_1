9f7ae1e7803956a2ffc304b01225d07ff16b2d39
Delaney McKnight <delaneymcknight@Delaneys-MacBook-Pro-2.local>
2020-05-16 19:18:16 -0700

    Background image, image, jscript attempt 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-console.log('Hello from js/scripts.js!');
-
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -7,9 +5,25 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for(let i=1; i<=5; i++){
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic'+i+'.jpg');
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(e){
+  displayedImage.src = e.target.src;
+  }
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick=function(){
+  const btnItem=btn.getAttribute('class');
+  if(btnItem==='dark'){
+    btn.setAttribute('class','light');
+    btn.textContent='Lighten';
+    overlay.style.backgroundColor="rgba(0,0,0,0.5)";
+  }else{
+    btn.setAttribute('class','dark');
+    btn.textContent='Darken';
+    overlay.style.backgroundColor="rgba(0,0,0,0)";
+  }
+}
